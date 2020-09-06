b65b4a46f12b809e375053aca9a32e97b14ba875
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-09-06 21:29:50 +0200

    .emacs.d: paths: store autosaves in global directory

--- .emacs.d/lisp/user-init-paths.el
+++ .emacs.d/lisp/user-init-paths.el
@@ -1,3 +1,4 @@
-(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
+(setq backup-directory-alist `((".*" . ,(concat user-emacs-directory "backups/"))))
+(setq auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "autosave/") t)))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
