5b801e5cf1a3f3ee95e77b829a3048e14fe91a8a
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-09-08 18:46:23 +0200

    .emacs.d: keys: create a ctrl f/g search

--- .emacs.d/lisp/user-init-keys.el
+++ .emacs.d/lisp/user-init-keys.el
@@ -25,28 +25,27 @@
 (global-set-key "\C-s" 'save-buffer)
 (define-key global-map (kbd "<S-down-mouse-1>") 'mouse-save-then-kill)
 
+; don't copy selections automatically
+(setq mouse-drag-copy-region nil)
+
 ; isearch
 (global-set-key "\C-r" nil)
-(global-set-key "\C-w" 'isearch-backward)
-(global-set-key "\C-e" 'isearch-forward)
-(global-set-key "\M-\C-w" 'isearch-backward-regexp)
-(global-set-key "\M-\C-e" 'isearch-forward-regexp)
+(global-set-key "\M-\C-r" nil)
+(global-set-key "\M-\C-s" nil)
+(global-set-key "\C-f" 'user-isearch-with-region)
+(global-set-key "\M-\C-f" 'isearch-forward-regexp)
 (add-hook 'isearch-mode-hook
     (lambda ()
+        (define-key isearch-mode-map "\C-g" 'isearch-repeat-forward)
+        (define-key isearch-mode-map [?\C-\S-g] 'isearch-repeat-backward)
+
         (define-key isearch-mode-map "\C-r" nil)
         (define-key isearch-mode-map "\C-s" nil)
         (define-key isearch-mode-map "\M-\C-r" nil)
         (define-key isearch-mode-map "\M-\C-s" nil)
-
-        (define-key isearch-mode-map "\C-w" 'isearch-repeat-backward)
-        (define-key isearch-mode-map "\C-e" 'isearch-repeat-forward)
-        (define-key isearch-mode-map "\M-\C-w" 'isearch-repeat-backward)
-        (define-key isearch-mode-map "\M-\C-e" 'isearch-repeat-forward)
+        (define-key isearch-mode-map "\C-v" 'user-isearch-paste)
     )
 )
-
-; don't copy selections automatically
-(setq mouse-drag-copy-region nil)
 
 ; isearch selected region, unhighlight everything when there's no selection
 ; based on: https://stackoverflow.com/a/32002122/2035624
