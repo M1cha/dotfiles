44901458fdb75adae3a9f316fa12e20ecba15d9c
Michael Zimmermann <sigmaepsilon92@gmail.com>
2023-06-27 21:44:57 +0200

    nvim: rewrite 👻
    - lua FTW
    - I switched from nvim-qt to running in the terminal. Having one global
      instance sounded awesome but ended up in a huge mess when opening tons
      of files from different projects. Now, I'll use the default/intended
      behavior of startin nvim for ever project in a terminal within it's
      project root. I can use tmux to open a vertically split terminal for
      building and running said project. That also removes the need for
      sshfs for editing remote files. neovim can use tmux as a clipboard
      manager which seems to send the contents over SSH to alacrity by
      default.
    - I make more use of default keybindings now

---
+++ .config/nvim/init.lua
@@ -0,0 +1,198 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Change leader key. Needs decreasing timeouts
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+vim.o.updatetime = 250
+
+vim.o.clipboard = "unnamedplus"
+vim.o.mouse = "a"
+vim.o.termguicolors = true
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes'
+vim.o.showmode = false
+vim.o.cursorline = true
+vim.o.undofile = true
+
+-- show whitespace
+--vim.cmd.match "ExtraWhitespace" "/\\s\\+$/"
+vim.cmd[[match ExtraWhitespace /\s\+$/]]
+vim.wo.list = true
+vim.wo.listchars = "space:·,eol:$,tab:» ,precedes:<"   
+
+require("lazy").setup({
+    {
+        "EdenEast/nightfox.nvim",
+        priority = 1000,
+    },
+
+    -- Git
+    "tpope/vim-fugitive",
+    "tpope/vim-rhubarb",
+
+    -- Detect tabstop and shiftwidth automatically
+    "tpope/vim-sleuth",
+
+    { "folke/which-key.nvim", opts = {} },
+    { "numToStr/Comment.nvim", opts = {} },
+
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                theme = "material",
+            },
+        },
+    },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+        },
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        build = ":TSUpdate",
+    },
+    -- "nvim-treesitter/playground",
+})
+
+require('nvim-treesitter.configs').setup {
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'vimdoc', 'vim' },
+    auto_install = false,
+    highlight = { enable = true },
+}
+require "nvim-treesitter.configs".setup {
+}
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldenable = false
+
+-- My color scheme
+local Shade = require("nightfox.lib.shade")
+local palettes = {
+    carbonfox = {
+        bg1 = "#000000",
+        fg0 = "#ffffff",
+        fg1 = "#ffffff",
+        fg2 = "#ffffff",
+        fg3 = "#ffffff",
+        comment = "#0088ff",
+        pink = "#ff9d00",
+        white = "#ffffff",
+        magenta = Shade.new("#BE95FF", 0.15, -0.15),
+    },
+}
+local specs = {
+    carbonfox = {
+        syntax = {
+            preproc = "#ff9d00",
+            field = palettes.carbonfox.white,
+            string = "#3ad900",
+            type = "#80ffbb",
+            number = "#ffee80",
+            const = "#ffee80",
+            builtin0 = "#ffffff",
+            builtin1 = "#80ffbb",
+            builtin2 = "#ffee80",
+        }
+    }
+}
+local groups = {
+    carbonfox = {
+        LineNr = { fg = "#0065bf", bg = "#000d1a" },
+        CursorLine = { bg = "#003b70" },
+        CursorLineNr = { fg = "#0065bf", bg = "#000d1a", style = "bold" },
+        Whitespace = { fg = "#777777" },
+        ExtraWhitespace = { fg = "#ffffff", bg = "#ff0044" },
+        Visual = { fg = "#ffffff", bg = "#0088ff" },
+        ["@parameter"] = { fg = "#ffffff" },
+        ["@keyword.return"] = { fg = palettes.carbonfox.magenta },
+        ["@include"] = { style = "bold", fg = "#80ffbb" },
+    }
+}
+require('nightfox').setup({
+    palettes = palettes,
+    specs = specs,
+    groups = groups,
+    options = {
+        styles = {
+            keywords = "bold",
+            functions = "bold",
+            comments = "bold",
+            conditionals = "bold",
+        }
+    }
+})
+vim.cmd[[colorscheme carbonfox]]
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+vim.keymap.set("n", "<leader>fc", builtin.live_grep, { desc = "[F]ind [C]ontents" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind [W]ord" })
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+
+vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "[G]it [F]iles" })
+vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "[G]it [C]ommits" })
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "[G]it [B]ranches" })
+vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "[G]it [S]tatus" })
+
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', '<up>', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', '<down>', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
