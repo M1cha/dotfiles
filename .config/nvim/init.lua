90fac9fe8878002f64d1273d401437e2ffa00996
Michael Zimmermann <sigmaepsilon92@gmail.com>
2025-02-09 07:28:49 +0100

    nvim: remove octo.nvim
    I don't really use it.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -96,17 +96,6 @@ require("lazy").setup({
     -- "nvim-treesitter/playground",
 
     {
-        "pwntester/octo.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim",
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-        },
-    },
-
-    {
         "phaazon/hop.nvim",
         opts = {},
     },
